1a18112b75a6518430aa8c691951cd2d5721bd54
Romain Pellerin <contact@romainpellerin.eu>
2017-02-07 17:48:20 -0800

    Restructure entire repository

---
+++ plain-javascript.js
@@ -0,0 +1,19 @@
+function nbOfPrimesFound (nb) {
+  var isPrime, i, j, counter = 1
+
+  for (i = 3; i <= nb; i = i + 2) {
+    isPrime = true
+
+    for (j = 3; j <= i / 2; j = j + 2) {
+      if (i % j === 0) {
+        isPrime = false
+        break
+      }
+    }
+
+    if (isPrime) {
+      counter++
+    }
+  }
+  return counter
+}
