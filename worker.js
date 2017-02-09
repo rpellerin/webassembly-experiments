a536eedeead5fb997b37066e6cdeacb52cf192e8
Romain Pellerin <contact@romainpellerin.eu>
2017-02-09 10:42:33 -0800

    Implement wasm benchmark

--- worker.js
+++ worker.js
@@ -1,9 +1,9 @@
 importScripts('plain-javascript.js', 'build/build-asm.asm.js', 'load-wasm.js')
 const asm_nbOfPrimesFound = asmCall._nbOfPrimesFound
-const wasm_nbOfPrimesFound = () => 'not implemented'
 
-const functionsToBench = {plain_nbOfPrimesFound, asm_nbOfPrimesFound, wasm_nbOfPrimesFound}
-onmessage = function({data}) {
+const functionsToBench = {plain_nbOfPrimesFound, asm_nbOfPrimesFound}
+
+const listenerBenchmarks = function({data}) {
   for(var name in functionsToBench) {
     const startTime = performance.now()
     const result = functionsToBench[name](data)
@@ -11,3 +11,22 @@ onmessage = function({data}) {
     postMessage({name, timeTaken, result})
   }
 }
+
+const listenerStart = function() {
+  loadWebAssembly('build/build-wasm.wasm')
+    .then( instance => {
+      var exports = instance.exports // the exports of that instance
+      functionsToBench.wasm_nbOfPrimesFound = exports._nbOfPrimesFound // our function
+    }
+    )
+    .catch( error => {
+      const wasm_nbOfPrimesFound = () => error.message
+      functionsToBench.wasm_nbOfPrimesFound = wasm_nbOfPrimesFound
+    })
+    .then( () => {
+      this.onmessage = listenerBenchmarks
+      postMessage({ ready: true })
+    })
+}
+
+onmessage = listenerStart
