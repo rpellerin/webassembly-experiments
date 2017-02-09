eb02bd5a7b58fc03654063004c95a939411f3d26
Romain Pellerin <contact@romainpellerin.eu>
2017-02-09 10:42:33 -0800

    Implement wasm benchmark

--- public/load-wasm.js
+++ public/load-wasm.js
@@ -1,5 +1,29 @@
 // https://gist.github.com/kripken/59c67556dc03bb6d57052fedef1e61ab
 
+var asm2wasmImports = { // special asm2wasm imports
+    "f64-rem": function(x, y) {
+      return x % y;
+    },
+    "f64-to-int": function(x) {
+      return x | 0;
+    },
+    "i32s-div": function(x, y) {
+      return ((x | 0) / (y | 0)) | 0;
+    },
+    "i32u-div": function(x, y) {
+      return ((x >>> 0) / (y >>> 0)) >>> 0;
+    },
+    "i32s-rem": function(x, y) {
+      return ((x | 0) % (y | 0)) | 0;
+    },
+    "i32u-rem": function(x, y) {
+      return ((x >>> 0) % (y >>> 0)) >>> 0;
+    },
+    "debugger": function() {
+      debugger;
+    },
+  };
+
 function loadWebAssembly (filename, imports) {
   // Fetch the file and compile it
   return fetch(filename)
@@ -9,6 +33,7 @@ function loadWebAssembly (filename, imports) {
       // Create the imports for the module, including the
       // standard dynamic library imports
       imports = imports || {}
+      imports.asm2wasm = asm2wasmImports
       imports.env = imports.env || {}
       imports.env.memoryBase = imports.env.memoryBase || 0
       imports.env.tableBase = imports.env.tableBase || 0
@@ -22,10 +47,3 @@ function loadWebAssembly (filename, imports) {
       return new WebAssembly.Instance(module, imports)
     })
 }
-// Main part of this example, loads the module and uses it.
-const pouet = loadWebAssembly('build/build-wasm.wasm')
-  .then(instance => {
-    var exports = instance.exports // the exports of that instance
-    return exports._nbOfPrimesFound // our function
-  }
-);
